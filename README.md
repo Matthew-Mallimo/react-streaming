01d0c23db032ca8e683d2f3f8177c53d79c8c376
Matthew Mallimo <matthew.mallimo@gmail.com>
2023-10-20 19:32:36 -0400

    feat: initial setup

---
+++ README.md
@@ -0,0 +1,10 @@
+## React Streaming Setup
+
+This is a small example of how to handle react SSR Streaming.
+
+To see the chunked responses, run:
+
+```
+curl --raw http://localhost:3000 
+```
+
---
+++ server.js
@@ -0,0 +1,38 @@
+import React, { use, Suspense } from 'react';
+import { renderToPipeableStream } from 'react-dom/server';
+const express = require('express')
+const app = express()
+const port = 3000
+
+const Comp = () => {
+  use(new Promise((resolve) => {
+    setTimeout(() => resolve(), 3000)
+  }))
+  return (
+    <div>
+      <h1>Loaded!</h1>
+    </div>
+  )
+}
+
+const App = () => (
+  <div>
+    <h1>Hello!!</h1>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Comp />
+    </Suspense>
+  </div>
+)
+
+app.get('/', (request, response) => {
+  const { pipe } = renderToPipeableStream(<App />, {
+    onShellReady() {
+      response.setHeader('content-type', 'text/html');
+      pipe(response);
+    }
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
